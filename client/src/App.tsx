64e08cc3605995cf7611b9680b0bb77c80062ef7
andrewhwanpark <andrew.park@sv.cmu.edu>
2022-11-13 13:36:20 -0800

    Use dark mode by default

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,27 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Container } from "@mui/system";
 import React from "react";
 import Form from "./Form";
 import Nav from "./Nav";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <div>
-      <Nav />
-      <Container maxWidth="sm" sx={{ mt: "1rem" }}>
-        <Form />
-      </Container>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        <Nav />
+        <Container maxWidth="sm" sx={{ mt: "1rem" }}>
+          <Form />
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
